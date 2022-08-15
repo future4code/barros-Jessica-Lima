796c4409795eb9b87c4474c3639b9acecff10c2f
Jessica Oliveira de Lima <jessicalimabitt@gmail.com>
2022-08-14 21:28:32 -0300

    projeto-labex2

--- modulo4/projeto-labex/projeto-labex/src/componentes/AplicationForm/AplicationForm.js
+++ modulo4/projeto-labex/projeto-labex/src/componentes/AplicationForm/AplicationForm.js
@@ -14,24 +14,22 @@ export function AplicationForm(){
             <h1>Formulario de viagem</h1>
             <ContainerForm>
                 <main>
-                <select>
-                    <option>viagem</option>
-                    <option></option>
-                    <option></option>
-                    <option></option>
-                </select>
-                    <input placeholder="Nome"></input>
-                    <input placeholder="Idade"></input>
-                    <input placeholder="Texto de candidatura"></input>
-                    <input placeholder="Profissão"></input>
-                <select>
-                    <option>Escolha um país</option>
-                    <option></option>
-                    <option></option>
-                    <option></option>
-                </select>
-
-          
+                    <select>
+                        <option>viagem</option>
+                        <option></option>
+                        <option></option>
+                        <option></option>
+                    </select>
+                        <input placeholder="Nome"></input>
+                        <input placeholder="Idade"></input>
+                        <input placeholder="Texto de candidatura"></input>
+                        <input placeholder="Profissão"></input>
+                    <select>
+                        <option>Escolha um país</option>
+                        <option></option>
+                        <option></option>
+                        <option></option>
+                    </select>
                 </main>
                 <button className="btnform">Enviar</button>
                 <button  className="btnform" onClick={goToLastPage} >Voltar</button>
