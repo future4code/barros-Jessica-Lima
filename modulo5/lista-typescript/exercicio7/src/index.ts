f986347754b5da1289c3de68ef918a6ee84373ad
Jessica Oliveira de Lima <jessicalimabitt@gmail.com>
2022-10-21 22:19:56 -0300

    lista-typescript-final

--- modulo5/lista-typescript/exercicio7/src/index.ts
+++ modulo5/lista-typescript/exercicio7/src/index.ts
@@ -6,6 +6,12 @@ type Estoque ={
     valorUnitario: number
 }
 
+type EstoqueAjustado ={
+    nome: string,
+    quantidade: number,
+    valorUnitario: number | string
+}
+
 const estoqueAtual: Estoque[] =[
 	{ nome: "MacMugffin", quantidade: 37, valorUnitario: 51.040},
 	{ nome: "Vassoura voadora", quantidade: 56, valorUnitario: 210.0},
@@ -16,8 +22,22 @@ const estoqueAtual: Estoque[] =[
 	{ nome: "Pokebola", quantidade: 200, valorUnitario: 99.9915}
 ]
 
-const ajustaPreco = (preco :number): string => {
-	const valorAjustado: string = preco.toFixed(2).replace('.', ',')
-	return "R$ "+valorAjustado
-}
-
+function ajustaValor(estoqueAtual:Estoque[]){
+	const ajusta = estoqueAtual.map((i)=>{
+		const valorAjustado: number | string = i.valorUnitario.toFixed(2).replace('.',',')
+		return {nome: i.nome, quantidade: i.quantidade, valorUnitario: "R$ "+valorAjustado}
+	})
+	let newEstoque :EstoqueAjustado[] = ajusta
+	
+	function ordenar(a: Estoque, b: Estoque){
+		if(a.quantidade <b.quantidade){
+			return -1
+		} else if(a.quantidade > b.quantidade){
+			return 1
+		}else{
+			return 0
+		}
+	}
+	return newEstoque.sort(ordenar)
+}	
+console.table(ajustaValor(estoqueAtual))
